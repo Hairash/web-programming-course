04109286a68350d4c8a1d4388a34eef259f2b176
Hairash <maslevian@yandex.ru>
2025-04-16 22:22:05 +0200

    Add lesson 6

---
+++ 6 client/server.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, render_template, jsonify
+from werkzeug.utils import redirect
+
+app = Flask('Hospital')
+
+appointments = []
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html', appointments=appointments)
+
+@app.route('/appointments', methods=['GET', 'POST'])
+def handle_appointments():
+    if request.method == 'POST':
+        # Handle the form submission
+        surname = request.data.decode('utf-8')
+        print('Received surname:', surname)
+        # Here you would typically save the appointment to a database
+        appointments.append(surname)
+        # Should return smth to be able to display it on client
+        return appointments
+    elif request.method == 'GET':
+        return appointments
+
+
+
+@app.route('/about', methods=['GET'])
+def about():
+    return render_template('about.html')
+
+app.run(debug=True, port=5001)
